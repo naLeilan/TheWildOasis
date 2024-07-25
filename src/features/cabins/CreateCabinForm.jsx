68f822ea9514509b568c1d5c7cb4e8fa5b06ca86
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-25 11:56:23 +0300

    added formrow

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,44 +10,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
-
 function CreateCabinForm() {
-  const { register, handleSubmit, reset, getValues } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
 
   const queryClient = useQueryClient();
 
@@ -78,6 +43,7 @@ function CreateCabinForm() {
           id="name"
           {...register("name", { required: "This field is required" })}
         />
+        {errors?.name?.message && <Error>{errors.name.message} </Error>}
       </FormRow>
 
       <FormRow>
