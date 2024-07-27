50e6edbba48eb690bfc9efcc8b287edaa19c917b
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-27 10:41:56 +0300

    added upload image in supabase storage

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -26,7 +26,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -104,7 +104,12 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow lable="Cabin Photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          type="file"
+          {...register("image", { required: "This field is required" })}
+        />
       </FormRow>
 
       <FormRow>
