988055e774278fe0c56769879fbdf8720cf93e2b
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-30 12:41:21 +0300

    added useSettings custom ook

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -85,7 +85,7 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={handleDuplicate}>
+          <button onClick={handleDuplicate} disabled={isCreating}>
             <HiSquare2Stack />
           </button>
           <button onClick={() => setShowForm((show) => !show)}>
