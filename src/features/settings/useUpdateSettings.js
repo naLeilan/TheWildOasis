a98b4f64790c3ee04da26e5e272444dfeb6b1624
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-30 13:14:40 +0300

    added useUpdatSettings custom hook

--- src/features/settings/useUpdateSettings.js
+++ src/features/settings/useUpdateSettings.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 import { updateSetting as updateSettingApi } from "../../services/apiSettings";
 
-export function useUpdateSetting() {
+export function useUpdateSettings() {
   const queryClient = useQueryClient();
 
   const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
